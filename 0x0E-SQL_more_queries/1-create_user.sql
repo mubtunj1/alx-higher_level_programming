a68b77db37426d7491d0e6d4ebe983b62669a5f3
Mubtunj <mubtunji01@gmail.com>
2023-03-15 23:01:22 +0100

    create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 --write script that create the MYSQL server user user_0d_1
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
