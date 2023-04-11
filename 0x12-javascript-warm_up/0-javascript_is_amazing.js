131ec7576415f8a4d85d663138a20cac708b2313
Mubtunj1 <mubtunji01@gmail.com>
2023-04-11 22:07:44 +0100

    Javascript is amazing

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -4,5 +4,5 @@
 //You must use console.log(...) to print all output
 //You are not allowed to use var
 
-const myVar = "JavaScript is amazing";
+const myVar = 'JavaScript is amazing';
 console.log(myVar);
