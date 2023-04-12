9c8a1ed3f83e5acfe21aa9765bf01bca06e14251
Mubtunj1 <mubtunji01@gmail.com>
2023-04-12 20:33:48 +0100

    improved

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-const myArray = ["C is fun", "Python is cool", "JavaScript is amazing"];
-myArray.forEach(element => console.log(element));
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+myArray.forEach(element => {
+  console.log(element);
+});
