61e8e4e70b901385ce9bb7cf7aaeb33966a207ed
Mubtunj1 <mubtunji01@gmail.com>
2023-04-12 20:20:49 +0100

    multi language

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
 
 const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let i = 0;
-while (i < myArray.length) {
-  console.log(myArray[i]);
-  i++;
-}
+myArray.forEach(element => console.log(element));
