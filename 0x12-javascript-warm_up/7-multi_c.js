2da7480f6fe1bfa688372f49f9136ca6214467e4
Mubtunj1 <mubtunji01@gmail.com>
2023-04-12 21:10:41 +0100

    Task 8 & 9

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const num = process.argv[2];
+const x = process.argv[2];
 
-if (!parseInt(num)) {
+if (!parseInt(x)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < num; i++) {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
 }
