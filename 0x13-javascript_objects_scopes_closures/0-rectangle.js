f9ea69c5682889594312001d353be4ab6c18c5e6
Mubtunj1 <mubtunji01@gmail.com>
2023-04-13 04:02:58 +0100

    Task 0-3

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/**
+ * Represents rectangle
+ */
+class Rectangle {}
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/**
+ * Represents rectangle with width and height
+ */
+class rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = rectangle;
