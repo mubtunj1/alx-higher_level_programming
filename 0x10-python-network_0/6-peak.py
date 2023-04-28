1cd1800d8c1c90dc5d4605a400cc99f18b403aab
Mubtunj1 <mubtunji01@gmail.com>
2023-04-28 01:58:06 +0100

    improved task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
 """Finds the peak in a list of unsorted integers"""
 def find_peak(list_of_integers):
-    size = len(list_of_integers)
-    middle = size
-    mid = size // 2
-    
-    if size == 0:
-        return None
-    while True:
-        middle = middle//2
-        if (mid < size - 1 and list_of_integers[mid] < list_of_integers[mid + 1]):
-            if middle // 2 == 0:
-                middle = 2
-            mid = mid + middle // 2
-        elif (mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]):
-            if middle // 2 == 0:
-                middle = 2
-            mid = mid - middle // 2
+    start = 0
+    end = len(list_of_integers) - 1
+
+    while start < end:
+        mid = (start + end) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            end = mid
         else:
-            return list_of_integers[mid]
+            start = mid + 1
+
+    return list_of_integers[start]
